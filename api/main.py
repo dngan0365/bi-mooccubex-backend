59ad3c56b92a4dac4df5ad55dfc2205b44ec8381
Ngân Đặng <dngan0365@gmail.com>
2025-05-11 12:06:36 +0700

    fix actions

--- api/main.py
+++ api/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+
+app = FastAPI(
+    title="bi-mooccubex-backend",
+    description="Backend for MOOC-Cubex",
+    version="0.1.0",
+)
+# Setup CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Include routers
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(bi_overview, prefix="/overview", tags=["overview"])
+app.include_router(bi_data_quality, prefix="/quality", tags=["quality"])
+app.include_router(bi_data_mining, prefix="/mining", tags=["mining"])
+app.include_router(bi_course, prefix="/course", tags=["course"])
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
