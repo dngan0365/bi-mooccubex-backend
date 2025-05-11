62ee6e9662ecccd1bbcb006b3ce0303d985a796f
Ngân Đặng <dngan0365@gmail.com>
2025-05-11 12:10:11 +0700

    fix main

--- api/main.py
+++ api/main.py
@@ -17,11 +17,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # Include routers
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(bi_overview, prefix="/overview", tags=["overview"])
-app.include_router(bi_data_quality, prefix="/quality", tags=["quality"])
-app.include_router(bi_data_mining, prefix="/mining", tags=["mining"])
-app.include_router(bi_course, prefix="/course", tags=["course"])
+# app.include_router(auth_router, prefix="/auth", tags=["auth"])
+# app.include_router(bi_overview, prefix="/overview", tags=["overview"])
+# app.include_router(bi_data_quality, prefix="/quality", tags=["quality"])
+# app.include_router(bi_data_mining, prefix="/mining", tags=["mining"])
+# app.include_router(bi_course, prefix="/course", tags=["course"])
 
 @app.get("/")
 async def root():
