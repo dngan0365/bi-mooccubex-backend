6789d7bfbccc22732f995e8ac544322d5986d927
Ngân Đặng <dngan0365@gmail.com>
2025-05-13 17:26:49 +0700

    add region

--- api/db/dynamodb.py
+++ api/db/dynamodb.py
@@ -4,9 +4,9 @@ from api.models.user import UserInDB
 import os
 
 dynamodb = boto3.resource("dynamodb", 
-                        #   region_name=os.getenv("AWS_REGION", "us-east-1"),
-                        #   aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-                        #   aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+                          region_name=os.getenv("AWS_REGION", "us-east-1"),
+                        # aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                        # aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
                           )
 
 user_table = dynamodb.Table("user")
