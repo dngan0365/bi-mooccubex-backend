f2704b0124d7524467747754c9785a595b0c9011
Ngân Đặng <dngan0365@gmail.com>
2025-05-13 23:40:14 +0700

    fix credential

--- api/db/dynamodb.py
+++ api/db/dynamodb.py
@@ -4,9 +4,7 @@ from api.models.user import UserInDB
 import os
 
 dynamodb = boto3.resource("dynamodb", 
-                          region_name=os.getenv("AWS_REGION", "us-east-1"),
-                            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-                            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+                          region_name= "us-east-1"
                           )
 
 user_table = dynamodb.Table("user")
